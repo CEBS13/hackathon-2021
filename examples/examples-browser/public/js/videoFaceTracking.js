9806aa8fa6f19688eba4168b651145f8df0bd98e
Carlos Augusto Valladares Diaz <cvalladares@loe.corp>
2021-05-21 13:51:04 -0600

    Changed icons

--- examples/examples-browser/public/js/videoFaceTracking.js
+++ examples/examples-browser/public/js/videoFaceTracking.js
@@ -219,7 +219,7 @@ var app = angular.module('myApp', []);
     }
 
     function checkEmotion(){
-      if($scope.emotionsCount.neutral === 50 || $scope.emotionsCount.sad === 10){
+      if($scope.emotionsCount.angry === 2 || $scope.emotionsCount.sad === 10){
         console.log("hello")
         $('#modal60').modal('open');
         $('#playPauseBtn').text('Play');
